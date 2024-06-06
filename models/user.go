48075215b10b1043424733070084801d34432ddc
Dragon00716 <pmg76dayo@gmail.com>
2024-06-06 12:19:53 +0900

    ユーザーデータ構造変更

--- models/user.go
+++ models/user.go
@@ -11,4 +11,13 @@ type User struct {
 	EmergencyContact string    `json:"emergency_contact" firestore:"emergency_contact"`
 	WorkContact      string    `json:"work_contact" firestore:"work_contact"`
 	BloodType        string    `json:"blood_type" firestore:"blood_type"`
+
+	ChronicDisease       string `json:"chronic_disease" firestore:"chronic_disease"`
+	History              string `json:"history" firestore:"history"`
+	HospitalDestination  string `json:"hospital_destination" firestore:"hospital_destination"`
+	Department           string `json:"department" firestore:"department"`
+	PrimaryCareDoctor    string `json:"primary_care_doctor" firestore:"primary_care_doctor"`
+	MedicationManagement string `json:"medication_management" firestore:"medication_management"`
+	Allergy              string `json:"allergy" firestore:"allergy"`
+	VaccinationStatus    bool   `json:"vaccination_status" firestore:"vaccination_status"`
 }
