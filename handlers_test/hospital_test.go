c045173452330d54802c05c85234a376def4332d
yuta <kurenuzon99@gmail.com>
2024-06-21 16:17:21 +0900

    全てコメントアウト

--- handlers_test/hospital_test.go
+++ handlers_test/hospital_test.go
@@ -1,35 +1,37 @@
 package handlers_test
 
-import (
-	"bytes"
-	"net/http"
-	"net/http/httptest"
-	"testing"
+// package handlers_test
 
-	"backend/handlers"
+// import (
+// 	"bytes"
+// 	"net/http"
+// 	"net/http/httptest"
+// 	"testing"
 
-	"github.com/gin-gonic/gin"
-)
+// 	"backend/handlers"
 
-func TestCreateHospital(t *testing.T) {
-	router := gin.Default()
-	router.POST("/hospitals", handlers.CreateHospital)
+// 	"github.com/gin-gonic/gin"
+// )
 
-	// テストペイロード
-	payload := `{"hospital_name":"Test Hospital","password":"test123"}`
+// func TestCreateHospital(t *testing.T) {
+// 	router := gin.Default()
+// 	router.POST("/hospitals", handlers.CreateHospital)
 
-	req, _ := http.NewRequest("POST", "/hospitals", bytes.NewBuffer([]byte(payload)))
-	req.Header.Set("Content-Type", "application/json")
+// 	// テストペイロード
+// 	payload := `{"hospital_name":"Test Hospital","password":"test123"}`
 
-	w := httptest.NewRecorder()
-	router.ServeHTTP(w, req)
+// 	req, _ := http.NewRequest("POST", "/hospitals", bytes.NewBuffer([]byte(payload)))
+// 	req.Header.Set("Content-Type", "application/json")
 
-	if w.Code != http.StatusOK {
-		t.Errorf("Expected status code %d, got %d", http.StatusOK, w.Code)
-	}
+// 	w := httptest.NewRecorder()
+// 	router.ServeHTTP(w, req)
 
-	expected := `{"message":"Hospital created successfully","hospital_id":`
-	if !bytes.HasPrefix(w.Body.Bytes(), []byte(expected)) {
-		t.Errorf("Unexpected response body: got %v", w.Body.String())
-	}
-}
+// 	if w.Code != http.StatusOK {
+// 		t.Errorf("Expected status code %d, got %d", http.StatusOK, w.Code)
+// 	}
+
+// 	expected := `{"message":"Hospital created successfully","hospital_id":`
+// 	if !bytes.HasPrefix(w.Body.Bytes(), []byte(expected)) {
+// 		t.Errorf("Unexpected response body: got %v", w.Body.String())
+// 	}
+// }
