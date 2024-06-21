f1cc1c5aa19716896d1ebf019605404de719e8d3
yuta <kurenuzon99@gmail.com>
2024-06-21 16:15:08 +0900

    不要なモデルの削除

--- models/health_care_staff.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Health_care_staff struct {
-	StaffID   string `json:"staff_id" firestore:"staff_id"`
-	StaffName string `json:"Staff_name" firestore:"Staff_name"`
-	Password  string `json:"password" firestore:"password"`
-}
--- models/hospital.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Hospital struct {
-	HospitalID   string `json:"hospital_id" firestore:"hospital_id"`
-	HospitalName string `json:"hospital_name" firestore:"hosiital_name"`
-	Password     string `json:"password" firestore:"password"`
-}
