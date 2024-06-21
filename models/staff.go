00b530400f769fa9ff0fa61b2bac40658ee6761d
Dragon00716 <pmg76dayo@gmail.com>
2024-06-21 15:57:45 +0900

    データモデル変更

--- models/staff.go
+++ models/staff.go
@@ -1,7 +1,14 @@
 package models
 
 type Staff struct {
-	StaffID   string `json:"staff_id" firestore:"staff_id"`
-	StaffName string `json:"Staff_name" firestore:"Staff_name"`
-	Password  string `json:"password" firestore:"password"`
+	StaffID       string `json:"staff_id" firestore:"staff_id"`
+	StaffName     string `json:"Staff_name" firestore:"Staff_name"`
+	Password      string `json:"password" firestore:"password"`
+	Department    string `json:"department" firestore:"department"`         //部門
+	Position      string `json:"position" firestore:"position"`             //役職
+	Date          string `json:"date" firestore:"date"`                     //入社日
+	Boss          string `json:"boss" firestore:"boss"`                     //上司
+	DoctorMessage string `json:"Doctor_message" firestore:"Doctor_message"` //主治医メッセージ
+	WorkStatus    string `json:"workstatus" firestore:"workstatus"`         //仕事中か否か
+	//管理レベルは一旦保留
 }
