1b97c38d4279952642196cc6ea84c31608cb2201
Dragon00716 <pmg76dayo@gmail.com>
2024-06-27 11:15:23 +0900

    新規登録、user情報取得

--- main.go
+++ main.go
@@ -19,11 +19,16 @@ func main() {
 	r.POST("/users", handlers.CreateUser)
 	r.GET("/users/:id", handlers.GetUser)
 
+	r.POST("/medical_memos", handlers.CreateMedicalMemo)
+	r.GET("/medical_memos/:id", handlers.GetMedicalMemo)
+
+	// Hospitals routes
+	r.POST("/hospitals", handlers.CreateHospital)
+	r.GET("/hospitals/:id", handlers.GetHospital)
+
 	// 認証ルートを追加
 	r.POST("/login", handlers.LoginHandler)
-
-	// 障害者番号をチェックするルートを追加
-	r.POST("/checkDisabilityId", handlers.CheckDisabilityIdHandler)
+	r.POST("/checkDisabilityID", handlers.CheckDisabilityIdHandler)
 
 	r.Run()
 }
--- models/user.go
+++ models/user.go
@@ -5,20 +5,25 @@ type EmergencyContact struct {
 	Phone string `json:"phone" firestore:"phone"`
 }
 
+type History struct {
+	Memo string `json:"memo" firestore:"memo"`
+	Date string `json:"date" firestore:"date"`
+}
+
 type User struct {
-	UserID            string             `json:"user_id" firestore:"user_id"`                         //障がい者番号
-	UserName          string             `json:"user_name" firestore:"user_name"`                     //名前
-	Password          string             `json:"password" firestore:"password"`                       //パスワード
-	Age               int                `json:"age" firestore:"age"`                                 //年齢
-	Address           string             `json:"address" firestore:"address"`                         //住所
-	Photo             string             `json:"photo" firestore:"photo"`                             //本人写真
-	BirthDate         string             `json:"birth_date" firestore:"birth_date"`                   //生年月日
-	Contact           string             `json:"contact" firestore:"contact"`                         //本人連絡先
-	EmergencyContacts []EmergencyContact `json:"emergency_contacts" firestore:"emergency_contacts"`   //緊急連絡先
-	ChronicDisease    string             `json:"chronic_disease" firestore:"chronic_disease"`         //病名
-	History           []string           `json:"history" firestore:"history"`                         //倒れた履歴などの管理のため配列にする
-	Date              []string           `json:"date" firestore:"date"`                               //日付の管理をするために配列にする
-	PrimaryCareDoctor string             `json:"primary_care_doctor" firestore:"primary_care_doctor"` //かかりつけ医
-	Medication_status string             `json:"medication_statu" firestore:"medication_"`            //服薬中の薬
-	Doctor_Comment    string             `json:"doctor_comment" firestore:"doctor_comment"`           //主治医のコメント
+	UserID            string             `json:"user_id" firestore:"user_id"`                       //障がい者番号
+	UserName          string             `json:"user_name" firestore:"user_name"`                   //名前
+	Password          string             `json:"password" firestore:"password"`                     //パスワード
+	Age               int                `json:"age" firestore:"age"`                               //年齢
+	Address           string             `json:"address" firestore:"address"`                       //住所
+	Photo             string             `json:"photo" firestore:"photo"`                           //本人写真
+	BirthDate         string             `json:"birth_date" firestore:"birth_date"`                 //生年月日
+	Contact           string             `json:"contact" firestore:"contact"`                       //本人連絡先
+	EmergencyContacts []EmergencyContact `json:"emergency_contacts" firestore:"emergency_contacts"` //緊急連絡先
+	ChronicDisease    string             `json:"chronic_disease" firestore:"chronic_disease"`       //病名
+	Historys          []History          `json:"historys" firestore:"historys"`                     //倒れた履歴などの管理のため配列にする
+	// Date              []string           `json:"date" firestore:"date"`                               //日付の管理をするために配列にする
+	PrimaryCareDoctor string `json:"primary_care_doctor" firestore:"primary_care_doctor"` //かかりつけ医
+	MedicationStatus  string `json:"medicationstatu" firestore:"medicationstatus"`        //服薬中の薬
+	DoctorComment     string `json:"doctorcomment" firestore:"doctorcomment"`             //主治医のコメント
 }
