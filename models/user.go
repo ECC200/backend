4ff72fafafe5fcef3abf0193626fdbc804c56872
yuta <kurenuzon99@gmail.com>
2024-07-01 14:46:15 +0900

    スペル修正

--- models/user.go
+++ models/user.go
@@ -11,8 +11,6 @@ type History struct {
 }
 
 type User struct {
-
-
 	UserID            string             `json:"user_id" firestore:"user_id"`                         //障がい者番号
 	UserName          string             `json:"user_name" firestore:"user_name"`                     //名前
 	Password          string             `json:"password" firestore:"password"`                       //パスワード
@@ -25,6 +23,6 @@ type User struct {
 	ChronicDisease    string             `json:"chronic_disease" firestore:"chronic_disease"`         //病名
 	Historys          []History          `json:"historys" firestore:"historys"`                       //倒れた履歴などの管理のため配列にする
 	PrimaryCareDoctor string             `json:"primary_care_doctor" firestore:"primary_care_doctor"` //かかりつけ医
-	Medication_status string             `json:"medication_statu" firestore:"medication_"`            //服薬中の薬
+	Medication_status string             `json:"medication_status" firestore:"medication_status"`     //服薬中の薬
 	Doctor_Comment    string             `json:"doctor_comment" firestore:"doctor_comment"`           //主治医のコメント
 }
