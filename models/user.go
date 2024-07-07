f08b6317d334fa793b94a137bcea9c6d62333498
Fryoken <106068795+Fryoken@users.noreply.github.com>
2024-07-07 12:29:52 +0900

    完成 user.go

--- models/user.go
+++ models/user.go
@@ -14,7 +14,7 @@ type User struct {
 	UserID              string             `json:"user_id" firestore:"user_id"`                           //障がい者番号
 	UserName            string             `json:"user_name" firestore:"user_name"`                       //名前
 	Password            string             `json:"password" firestore:"password"`                         //パスワード
-	Age                 int                `json:"age" firestore:"age"`                                   //年齢
+	Age                 string             `json:"age" firestore:"age"`                                   //年齢
 	Address             string             `json:"address" firestore:"address"`                           //住所
 	Photo               string             `json:"photo" firestore:"photo"`                               //本人写真
 	BirthDate           string             `json:"birth_date" firestore:"birth_date"`                     //生年月日
@@ -22,9 +22,10 @@ type User struct {
 	EmergencyContacts   []EmergencyContact `json:"emergency_contacts" firestore:"emergency_contacts"`     //緊急連絡先
 	ChronicDisease      string             `json:"chronic_disease" firestore:"chronic_disease"`           //病名
 	Historys            []History          `json:"historys" firestore:"historys"`                         //倒れた履歴などの管理のため配列にする
-	HospitalDestination string             `json:"hospital_destination" firestore:"hospital_destination"` //かかりつけ病院
 	PrimaryCareDoctor   string             `json:"primary_care_doctor" firestore:"primary_care_doctor"`   //かかりつけ医
-	Medication_status   string             `json:"medication_status" firestore:"medication_status"`       //服薬中の薬
-	Doctor_Comment      string             `json:"doctor_comment" firestore:"doctor_comment"`             //主治医のコメント
-	Specialty           string             `json:"specialty" firestore:"specialty"`
+	MedicationStatus    string             `json:"medication_status" firestore:"medication_status"`       //服薬中の薬
+	DoctorComment       string             `json:"doctor_comment" firestore:"doctor_comment"`             //主治医のコメント
+	HospitalDestination string             `json:"hospital_destination" firestore:"hospital_destination"` //かかりつけの病院
+	Specialty           string             `json:"specialty" firestore:"specialty"`                       //何科
+	DisabilityGrade     string             `json:"disability_grade" firestore:"disability_grade"`         //障がい者等級
 }
